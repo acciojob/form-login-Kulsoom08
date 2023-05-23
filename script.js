be2e57b23baf635d71362e00c4ee9df319412a9d
Kulsoom08 <120732404+Kulsoom08@users.noreply.github.com>
2023-05-23 22:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstname=document.querySelector("#fname").value;
+	let secondname=document.querySelector("#lname").value;
+alert(firstname,secondname);
 }
